0bb617468a1632ad5c6bf79cfab5b0ad515c5461
UTKARSH SHARMA <46118972+utkarsh85458492@users.noreply.github.com>
2023-08-23 10:31:57 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -83,7 +83,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
+				
 				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
