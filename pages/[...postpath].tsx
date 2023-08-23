46efc7210da377090aba2f036ba0ab5fd8306385
UTKARSH SHARMA <46118972+utkarsh85458492@users.noreply.github.com>
2023-08-23 10:52:10 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -83,13 +83,8 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
+			
 				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
